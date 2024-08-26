c5a6b4f748e6db1f95ff66ee0a056e0a5603b9c4
grizeus <32486511+grizeus@users.noreply.github.com>
2024-08-26 23:04:32 +0300

    Experimental env var (#8)

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,12 +2,12 @@
 
 import axios from "axios";
 
-const API_KEY = "34523545-f21683fd59bfc3e4e2549fe07";
 const BASE_URL = "https://pixabay.com/api";
 
 axios.defaults.baseURL = BASE_URL;
 
 export default async function fetchFrom(query) {
+  const API_KEY = process.env.API_KEY;
   const params = {
     key: API_KEY,
     q: String(query),
