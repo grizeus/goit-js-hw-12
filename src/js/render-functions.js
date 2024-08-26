0354bf211ed0c9f95ae0857bc011ae72ffb24ac3
grizeus <32486511+grizeus@users.noreply.github.com>
2024-08-26 14:34:21 +0300

    [FEAT] Add gallery rendering from pixabay (#6)
    * add markup an logic 
    * add style from blueprint, split CSS a bit
    * add loader, customize SLB

---
+++ src/js/render-functions.js
@@ -0,0 +1,49 @@
+"use strict";
+
+import simpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import "../css/custom-slb.css";
+
+const gallery = document.querySelector("ul.gallery-list");
+export function clearGallery() {
+  gallery.innerHTML = "";
+}
+
+export function renderGallery(fetchList) {
+  gallery.innerHTML = "";
+  const galleryMarkup = fetchList
+    .map(picture => {
+      const {
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      } = picture;
+      return `
+        <li class="gallery-item">
+            <a href="${largeImageURL}" target="_blank">
+                <div class="img-wrapper">
+                  <img class="gallery-image" src="${webformatURL}" alt="${tags}" />
+                </div>
+            </a>
+            <ul class="gallery-item-info">
+                <li class="gallery-item-likes"><b>Likes</b> ${likes}</li>
+                <li class="gallery-item-views"><b>Views</b> ${views}</li>
+                <li class="gallery-item-comments"><b>Comments</b> ${comments}</li>
+                <li class="gallery-item-downloads"><b>Downloads</b> ${downloads}</li>
+            </ul>
+        </li>
+        `;
+    })
+    .join("");
+
+  gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+  const lightbox = new simpleLightbox(".gallery-list a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}
--- src/main.js
+++ src/main.js
@@ -1,36 +1,44 @@
 "use strict";
 
-import fetchFrom from './js/pixabay-api.js';
-import iziToast from 'izitoast';
+import fetchFrom from "./js/pixabay-api.js";
+import { renderGallery, clearGallery } from "./js/render-functions.js";
+import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 import "./css/custom-izitoast.css";
-import errSvg from './img/error.svg';
+import errSvg from "./img/error.svg";
 
 const searchInput = document.querySelector(".search-input");
 const searchButton = document.querySelector(".search-btn");
+const loader = document.querySelector(".loader");
 
-searchButton.addEventListener("click", async (e) => {
-    e.preventDefault();
-    const searchQuery = searchInput.value;
-    if (searchQuery === "") {
-        return;
+searchButton.addEventListener("click", async e => {
+  e.preventDefault();
+  loader.classList.toggle("visually-hidden");
+  const searchQuery = searchInput.value;
+  if (searchQuery === "") {
+    if (!loader.classList.contains("visually-hidden")) {
+      loader.classList.toggle("visually-hidden");
     }
-    const res = await fetchFrom(searchQuery);
-    if (parseInt(res.totalHits) > 0) {
-        console.log(res);
-    } else {
-        console.log(
-          "Sorry, there are no images matching your search query. Please try again!"
-        );
-        iziToast.error({
-            titleSize: "16px",
-            message: "Sorry, there are no images matching your search query. Please try again!",
-            maxWidth: "432px",
-            position: "topRight",
-            closeOnEscape: true,
-            icon: "error",
-            iconUrl: errSvg,
-            theme: "dark",
-        });
-    }
+    clearGallery();
+    return;
+  }
+  const res = await fetchFrom(searchQuery);
+  loader.classList.toggle("visually-hidden");
+  if (parseInt(res.totalHits) > 0) {
+    console.log(res);
+    renderGallery(res.hits);
+  } else {
+    clearGallery();
+    iziToast.error({
+      titleSize: "16px",
+      message:
+        "Sorry, there are no images matching your search query. Please try again!",
+      maxWidth: "432px",
+      position: "topRight",
+      closeOnEscape: true,
+      icon: "error",
+      iconUrl: errSvg,
+      theme: "dark",
+    });
+  }
 });
