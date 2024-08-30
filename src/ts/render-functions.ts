4dc311b2ff32a5624bbcd87b673c0e42a5d29cb6
grizeus <zaytsewww@gmail.com>
2024-08-31 00:56:27 +0300

    fix render function to escape html entities in tags

--- src/ts/render-functions.ts
+++ src/ts/render-functions.ts
@@ -1,10 +1,12 @@
-interface Picture {
+import he from "he";
+
+export interface Picture {
   webformatURL: string;
   largeImageURL: string;
   tags: string[];
-  likes: number;
+  likes: number | undefined;
+  comments: number | undefined;
   views: number;
-  comments: number;
   downloads: number;
 }
 
@@ -24,11 +26,13 @@ export default function renderGallery(fetchData: FetchData) {
         comments,
         downloads,
       } = picture;
+
+      const escapedTags = he.escape(tags.join(', '));
       return `
         <li class="gallery-item">
-            <a href="${largeImageURL}" target="_blank">
+            <a href="${he.escape(largeImageURL)}" target="_blank">
                 <div class="img-wrapper">
-                  <img class="gallery-image" src="${webformatURL}" alt="${tags}" />
+                  <img class="gallery-image" src="${he.escape(webformatURL)}" alt="${escapedTags}" />
                 </div>
             </a>
             <ul class="gallery-item-info">
